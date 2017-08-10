513ff745b30ac18346431711beb13744c1805acb
Mārtiņš Ungurs <martins.ungurs@cube.lv>
2017-08-10 16:06:09 +0300

    [repo]: init

---
+++ src/PaymentsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Arbory\Payments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Omnipay\Omnipay;
+
+class PaymentsController extends Controller
+{
+    public function purchase($gatewayHandler, $transactionId = null){
+
+        //TODO: load from config available gateways
+        $gateways = [
+            'FirstDataLatvia' => []
+        ];
+
+        $test = Omnipay::create('FirstDataLatvia');
+
+        $supported = Omnipay::getSupportedGateways();
+
+        //$gateway = Omnipay::create();
+
+        dump($test);
+    }
+}
